7e51db4d5328cd3f67d825cc7b1f4ff63ab82514
John Smith <example@stanford.edu>
2013-07-16 07:34:53 +0000

    Read from Index.html

--- web.js
+++ web.js
@@ -3,7 +3,8 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+    fs.readFile('index.html',function(err,data){
+  response.send(data.toString());
 });
 
 var port = process.env.PORT || 5000;
